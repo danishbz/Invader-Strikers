14bfb6a53257bf53cd13573b471b31a0688bc659
DannyZachary <danishbzz@protonmail.com>
2023-08-25 18:25:34 +0800

    Spawning Enemies
    Changes added:
    -Set up players health
    -Make enemies hurt
    -waiting between hits
    -set tp a health bar
    -updating health bar
    -making enemy prefabs
    -spawning enemies
    -spawning enemies outside screen
    -minor fixes

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,15 +8,37 @@ public class EnemyController : MonoBehaviour
     public float moveSpeed;
     private Transform target;
 
+    public float damage;
+
+    public float hitWaitTime = 1f;
+    private float hitCounter;
+
     // Start is called before the first frame update
     void Start()
     {
         target = FindObjectOfType<PlayerController>().transform;
+        target = PlayerHealthController.instance.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         theRB.velocity = (target.position - transform.position).normalized * moveSpeed;
+
+        if(hitCounter > 0f)
+        {
+            hitCounter -= Time.deltaTime;
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && hitCounter <= 0f)
+        {
+            PlayerHealthController.instance.TakeDamage(damage);
+
+            hitCounter = hitWaitTime;
+        }
+    }
+
 }
