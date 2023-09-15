4acf6add53172bf20de8dfd6330d58f899174680
SanjayKsk <skhaira98@gmail.com>
2023-09-15 22:33:55 +0800

    Finished exploder enemy
    Finished exploder enemy

---
+++ Assets/Scripts/Exploder/ExploderController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ExploderController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float explosionDamage = 2f;
+    [SerializeField] private float explosionRadius = 3f;
+    private float explosionTimer = 1.25f;
+    private bool isImmobilized = false;
+    private Rigidbody2D rb;
+    private GameObject target;
+    private float hitCounter;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Move();
+
+    }
+
+    private void Move(){
+        if (target && !isImmobilized)
+        {
+            animator.SetBool("isWalking", true);
+            rb.velocity = (target.transform.position - transform.position).normalized * moveSpeed;
+
+            // Flip sprite when player posX is BIGGER than enemy posX
+            if (transform.position.x < target.transform.position.x)
+            {
+                spriteRenderer.flipX = true;
+            }
+            // Flip sprite when player posX is SMALLER than enemy posX
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        if (isImmobilized)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision){
+        if (collision.gameObject.tag == "Obstacle") 
+        {
+            GameObject obstacle = GameObject.FindGameObjectWithTag("Obstacle");   
+            Physics2D.IgnoreCollision(obstacle.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Explode();
+        }
+        
+    }
+
+    private void Explode(){
+        isImmobilized = true;
+        animator.SetBool("isWalking", false);
+        animator.SetTrigger("Explosion");
+        StartCoroutine(ExplodeCoroutine());
+    }
+
+    private void checkPlayerInRange(){
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject.CompareTag("Player"))
+            {
+                HealthManager.instance.ApplyDamage(explosionDamage);
+            }
+        }
+    }
+
+    IEnumerator ExplodeCoroutine(){
+        yield return new WaitForSeconds(explosionTimer);
+        checkPlayerInRange();
+        Destroy(gameObject);
+    }
+
+
+
+}
+
